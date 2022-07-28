2f7b091aa222f57455500af5ab0ddd1eda82ca37
Zack Rogers <zack.rogers.t@gmail.com>
2022-07-28 17:40:24 -0600

    added Palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Palindrome() {
-	return <p>Palindrome</p>;
+	const [userInput, setUserInput] = useState("");
+	const [palindrome, setPalindrome] = useState("");
+
+	function check(value) {
+		setPalindrome(value.split("").reverse().join("").toLowerCase() === value.toLowerCase() ? "true" : "false");
+	}
+	
+	return (
+		<div className="puzzleBox palindromePB">
+			<h4>Palindrome</h4>
+			<input className="inputLine" onChange={e => setUserInput(e.target.value)}></input>
+			<button className="confirmationButton" onClick={() => check(userInput)}>
+				Check
+			</button>
+			<span className="resultsBox">Result: {palindrome}</span>
+		</div>
+	);
 }
 
 export default Palindrome;
