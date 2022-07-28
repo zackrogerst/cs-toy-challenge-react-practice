4496048813107aa94ee6b09585b8ad8361e328ce
Zack Rogers <zack.rogers.t@gmail.com>
2022-07-28 15:57:07 -0600

    added EvenAndOdd logic both function and class based component syntax

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,7 +1,100 @@
-import React from "react";
+// import React, { Component } from "react";
+
+// export default class EvenAndOdd extends Component {
+// 	constructor() {
+// 		super();
+
+// 		this.state = {
+// 			evenArray: [],
+// 			oddArray: [],
+// 			userInput: ""
+// 		};
+// 	}
+
+// 	handleChange(val) {
+// 		this.setState({ userInput: val });
+// 	}
+
+// 	assignEvenAndOdds(userInput) {
+// 		const evens = userInput
+// 			.split(",")
+// 			.map((e) => parseInt(e, 10))
+// 			.filter((e) => e % 2 === 0);
+// 		const odds = userInput
+// 			.split(",")
+// 			.map((e) => parseInt(e, 10))
+// 			.filter((e) => e % 2 !== 0);
+
+// 		this.setState({ evenArray: evens, oddArray: odds });
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="puzzleBox evenAndOddPB">
+// 				<h4>Evens and Odds</h4>
+// 				<input
+// 					className="inputLine"
+// 					onChange={(e) => this.handleChange(e.target.value)}
+// 				></input>
+// 				<button
+// 					className="confirmationButton"
+// 					onClick={() => {
+// 						this.assignEvenAndOdds(this.state.userInput);
+// 					}}
+// 				>
+// 					Split
+// 				</button>
+// 				<span className="resultsBox">
+// 					Evens: {JSON.stringify(this.state.evenArray)}
+// 				</span>
+// 				<span className="resultsBox">
+// 					Odds: {JSON.stringify(this.state.oddArray)}
+// 				</span>
+// 			</div>
+// 		);
+// 	}
+// }
+
+import React, { useState } from "react";
 
 function EvenAndOdd() {
-	return <p>EvenAndOdd</p>;
+	const [evenArray, setEvenArray] = useState([]);
+	const [oddArray, setOddArray] = useState([]);
+	const [userInput, setUserInput] = useState("");
+
+	const getEven = (arr) =>
+		arr
+			.split(",")
+			.map((e) => parseInt(e, 10))
+			.filter((e) => e % 2 === 0);
+	const getOdd = (arr) =>
+		arr
+			.split(",")
+			.map((e) => parseInt(e, 10))
+			.filter((e) => e % 2 !== 0);
+
+	return (
+		<div className="puzzleBox evenAndOddPB">
+			<h4>Evens and Odds</h4>
+			<input
+				className="inputLine"
+				onChange={(e) => setUserInput(e.target.value)}
+			></input>
+			<button
+				className="confirmationButton"
+				onClick={() => {
+					setEvenArray(getEven(userInput));
+					setOddArray(getOdd(userInput));
+				}}
+			>
+				Split
+			</button>
+			<span className="resultsBox">
+				Evens: {JSON.stringify(evenArray)}
+			</span>
+			<span className="resultsBox">Odds: {JSON.stringify(oddArray)}</span>
+		</div>
+	);
 }
 
 export default EvenAndOdd;
